7f5f29561956477694eb469b7e0e88137e83719d
Adriano Pequeno <adrian.pekeno@gmail.com>
2023-08-29 20:36:13 -0300

    Modifying dynamic header data

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,34 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send("Home Page");
+    res.render("pages/page", {
+        banner: {
+            title: "Todos os animais",
+            background: "allanimals.jpg"
+        }
+    });
 };
 export const dogs = (req: Request, res: Response) => {
-    res.send("Home Dogs");
+    res.render("pages/page", {
+        banner: {
+            title: "Cachorros",
+            background: "banner_dog.jpg"
+        }
+    });
 };
 export const cats = (req: Request, res: Response) => {
-    res.send("Home Cats");
+    res.render("pages/page", {
+        banner: {
+            title: "Gatos",
+            background: "banner_cat.jpg"
+        }
+    });
 };
 export const fishes = (req: Request, res: Response) => {
-    res.send("Home Fishes");
+    res.render("pages/page", {
+        banner: {
+            title: "Peixes",
+            background: "banner_fish.jpg"
+        }
+    });
 };
